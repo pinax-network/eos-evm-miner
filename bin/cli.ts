67f7a0376aec158000d481d8f948a47a429e04e6
Denis Carriere <carriere.denis@gmail.com>
2023-06-09 16:48:30 -0400

    fix metrics disabled

--- bin/cli.ts
+++ bin/cli.ts
@@ -3,10 +3,10 @@
 import { Command } from "commander";
 import pkg from "../package.json" assert { type: "json" };
 import { DEFAULT_HOSTNAME, DEFAULT_LOCK_GAS_PRICE, DEFAULT_METRICS_DISABLED, DEFAULT_MINER_PERMISSION, DEFAULT_PORT, DEFAULT_PROMETHEUS_PORT } from "../src/config.js";
-import { start } from "../index.js";
 import { claim } from "./claim.js";
 import { open } from "./open.js";
 import { powerup } from "./powerup.js";
+import start from "../index.js";
 
 const program = new Command();
 program.name(pkg.name)
@@ -19,7 +19,7 @@ defaultOptions(program.command("start"))
     .option('-p --port <int>', 'JSON RPC listens on port number, listen for incoming Ethereum transactions.', String(DEFAULT_PORT))
     .option('--hostname <string>', `JSON RPC listens on hostname, listen for incoming Ethereum transactions (ex: "${DEFAULT_HOSTNAME})"`)
     .option('--metrics-listen-port <int>', 'The process will listen on this port for Prometheus metrics requests', String(DEFAULT_PROMETHEUS_PORT))
-    .option('--metrics-disabled', 'If set, will not send metrics to Prometheus', DEFAULT_METRICS_DISABLED)
+    .option('--metrics-disabled', 'If set, will not send metrics to Prometheus')
     .option('--verbose', 'Enable verbose logging')
     .option('--lock-gas-price', `Lock gas price as hex value (ex: "${DEFAULT_LOCK_GAS_PRICE}")`)
     .action(options => {
